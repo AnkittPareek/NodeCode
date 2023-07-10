a22133e1c86d5993059fc636cbc2371b4e44630f
Ankit Pareek <apareekh@eteaminc.com>
2023-07-10 16:45:23 +0530

    create Tour MVC

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -21,17 +21,23 @@ exports.getTour = (req, res) => {
   });
 };
 exports.createTour = async (req, res) => {
-  // const newTour = new Tour({});
-  // newTour.save();
+  try {
+    // const newTour = new Tour({});
+    // newTour.save();
+    const newTour = await Tour.create(req.body);
 
-  const newTour = awaitTour.create(req.body);
-
-  res.status(201).json({
-    status: 'success',
-    data: {
-      tour: newTour,
-    },
-  });
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 exports.updateTour = (req, res) => {
   res.status(200).json({
